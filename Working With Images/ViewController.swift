2459d8d4dbbf6649c5bdee8bfedc479c72979418
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-15 13:32:36 -0800

    Add animation effect to initial screen by calling "viewDidLayoutSubViews" and "viewDidAppear"

--- Working With Images/ViewController.swift
+++ Working With Images/ViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let imageName:[String] = ["Two@3x", "Three@3x", "Ten@3x", "Queen@3x", "King@3x"]
+    
+    var count = 0
+    
+    var frameLocAndSize:[CGFloat] = []
+
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBAction func updateButtonPressed(sender: UIButton) {
+        let newImage = UIImage(named: imageName[count])
+        image.image = newImage
+        ++count
+        if count == imageName.count {
+            count = 0
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +39,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // +---    Methods to create animation effect ---+
+    // +---------------------------------------------+
+    override func viewDidLayoutSubviews() {
+        // Step1.0 Fly in effect 
+        //         - moving the image out of the view intially
+        image.center = CGPointMake(image.center.x + 400, image.center.y - 200)
+        // Step1.1 Grandually show up 
+        //         - set initial transparency of the image (1=opaq, 0=transparent)
+        image.alpha = 0
+        // Step1.2 Grandually increase size
+        //         - set initial frame size of the view to 0
+        frameLocAndSize.append(image.frame.origin.x)
+        frameLocAndSize.append(image.frame.origin.y)
+        frameLocAndSize.append(image.frame.size.width)
+        frameLocAndSize.append(image.frame.size.height)
+        image.frame = CGRectMake(frameLocAndSize[0], frameLocAndSize[1], 0, 0)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        // Define animation duration and finish position
+        UIView.animateWithDuration(5, animations: { () -> Void in
+            // Step2.0 Fly in effect
+            //         - define the finish point of animation
+            self.image.center = CGPointMake(self.image.center.x - 400, self.image.center.y + 200)
+            // Step2.1 Gradually show up
+            //         - define the finish transparency
+            self.image.alpha = 1
+            // Step2.2 Gradually increase size
+            //         - set the finish frame size
+            self.image.frame = CGRectMake(self.frameLocAndSize[0] - 400, self.frameLocAndSize[1] + 200, self.frameLocAndSize[2], self.frameLocAndSize[3])
+        })
+    }
 }
 
